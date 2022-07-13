d2c5d88c86cb82b728b2d46a49387daf561ba2e7
Oskar Karlsson <Oskarkarlsson1993@gmail.com>
2022-07-13 16:08:59 +0200

    Improves api client error handling and typing (#52)

--- clients/web/src/hooks/user-actions.ts
+++ clients/web/src/hooks/user-actions.ts
@@ -6,32 +6,19 @@ export const useUserActions = () => {
   const [, setUser] = useRecoilState(currentUser);
 
   const register = async (firstName: string, lastName: string, email: string, password: string): Promise<void> => {
-    const authResponse = await ApiClient.register(firstName, lastName, email, password);
-
-    if (authResponse.status === 200) {
-      const user = await ApiClient.getMe();
-      setUser(user);
-    } else {
-      throw new Error('Registration failed');
-    }
+    await ApiClient.register(firstName, lastName, email, password);
+    const user = await ApiClient.getMe();
+    setUser(user);
   };
 
   const signIn = async (email: string, password: string): Promise<void> => {
-    const authResponse = await ApiClient.authnPassword(email, password);
-
-    if (authResponse.status === 200) {
-      const user = await ApiClient.getMe();
-      setUser(user);
-    } else {
-      throw new Error('Authentication failed');
-    }
+    await ApiClient.authnPassword(email, password);
+    const user = await ApiClient.getMe();
+    setUser(user);
   };
 
   const signOut = async () => {
-    const resp = await ApiClient.signOut();
-    if (resp.status !== 200) {
-      throw new Error('Failed to sign out');
-    }
+    await ApiClient.signOut();
   };
 
   return { register, signIn, signOut };
--- clients/web/src/lib/clients/api-client.ts
+++ clients/web/src/lib/clients/api-client.ts
@@ -1,106 +1,127 @@
-import { Project, ProjectInvitation, StripeSession, User } from '../../types';
+import ky, { HTTPError } from 'ky';
+import { Project, ProjectInvitation, User, StripeSession } from '../../types';
 
-const baseUrl = 'http://127.0.0.1:4000';
-
-const getOptions = {
-  method: 'GET',
-  mode: 'cors' as RequestMode,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  credentials: 'include' as RequestCredentials,
+type GetMeResponse = {
+  me: User;
 };
 
-const postOptions = {
-  method: 'POST',
-  mode: 'cors' as RequestMode,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  credentials: 'include' as RequestCredentials,
+type GetProjectResponse = {
+  project: Project;
 };
 
-const putOptions = {
-  method: 'PUT',
-  mode: 'cors' as RequestMode,
-  credentials: 'include' as RequestCredentials,
+type CreateProjectResponse = {
+  project: Project;
 };
 
-const deleteOptions = {
-  method: 'DELETE',
-  mode: 'cors' as RequestMode,
-  credentials: 'include' as RequestCredentials,
+type UpdateProjectResponse = {
+  project: Project;
 };
+
+type GetProjectUsersResponse = {
+  users: User[];
+};
+
+type CreateProjectInvitationResponse = {
+  project_invitation: ProjectInvitation;
+};
+
+type AcceptProjectInvitationResponse = {
+  accepted: boolean;
+};
+
+type ApiErrorInterface = {
+  name: string;
+  message: string;
+};
+
+type CreateStripeSessionResponse = {
+  stripe_session: StripeSession;
+};
+
+export class ApiError extends Error implements ApiErrorInterface {
+  public name: string;
+  constructor({ name, message }: { name: string; message: string }) {
+    super(message);
+    this.name = name;
+  }
+}
 
 export class ApiClient {
-  static async register(firstName: string, lastName: string, email: string, password: string): Promise<Response> {
-    return fetch(`${baseUrl}/authn/register`, {
-      ...postOptions,
-      body: JSON.stringify({ firstName, lastName, email, password }),
-    });
+  static client = ky.create({
+    prefixUrl: 'http://127.0.0.1:4000',
+    credentials: 'include',
+    mode: 'cors',
+  });
+
+  private static isCustomApiError(error: Record<string, unknown>) {
+    return ['name', 'message'].every((item) => error.hasOwnProperty(item));
   }
 
-  static async authnPassword(email: string, password: string): Promise<Response> {
-    return fetch(`${baseUrl}/authn/password`, {
-      ...postOptions,
-      body: JSON.stringify({ email, password }),
-    });
+  private static async handleApiError<T>(action: Promise<T>): Promise<T> {
+    try {
+      const res = await action;
+      return res;
+    } catch (err: unknown) {
+      if (err instanceof HTTPError) {
+        const body = await err.response.json();
+        if (body && this.isCustomApiError(body)) {
+          throw new ApiError({ name: body.name, message: body.message });
+        }
+      }
+
+      throw new ApiError({ name: 'UNKNOWN_ERROR', message: 'An unknown error occurred, please try again later' });
+    }
   }
 
-  static async signOut(): Promise<Response> {
-    return fetch(`${baseUrl}/authn/sign-out`, {
-      ...postOptions,
-    });
+  static async register(firstName: string, lastName: string, email: string, password: string): Promise<void> {
+    await this.handleApiError(this.client.post('authn/register', { json: { firstName, lastName, email, password } }));
+  }
+
+  static async authnPassword(email: string, password: string): Promise<void> {
+    await this.handleApiError(this.client.post('authn/password', { json: { email, password } }));
+  }
+
+  static async signOut(): Promise<void> {
+    await this.handleApiError(this.client.post('authn/sign-out'));
   }
 
   static async getMe(): Promise<User> {
-    return fetch(`${baseUrl}/me`, getOptions)
-      .then((response) => response.json())
-      .then((response) => response.me);
+    const { me } = await this.handleApiError(this.client.get('me').json<GetMeResponse>());
+    return me;
   }
 
   static async getProjects(): Promise<Project[]> {
-    return fetch(`${baseUrl}/projects`, getOptions)
-      .then((response) => response.json())
-      .then((response) => response.projects);
+    return this.handleApiError(this.client.get('projects').json());
   }
 
   static async createProject({ name }: { name: string }): Promise<Project> {
-    const { project } = await fetch(`${baseUrl}/projects`, {
-      ...postOptions,
-      body: JSON.stringify({ name }),
-    }).then((response) => response.json());
-
+    const { project } = await this.handleApiError(
+      this.client.post('projects', { json: { name } }).json<CreateProjectResponse>()
+    );
     return project;
   }
 
   static async getProject(id: string): Promise<Project> {
-    return fetch(`${baseUrl}/projects/${id}`, getOptions)
-      .then((response) => response.json())
-      .then((response) => response.project);
+    const { project } = await this.handleApiError(this.client.get(`projects/${id}`).json<GetProjectResponse>());
+    return project;
   }
 
   static async updateProject({ id, name }: { id: string; name: string }): Promise<Project> {
-    return fetch(`${baseUrl}/projects/${id}`, {
-      ...putOptions,
-      body: JSON.stringify({ name }),
-    })
-      .then((response) => response.json())
-      .then((response) => response.project);
+    const { project } = await this.handleApiError(
+      this.client.put(`projects/${id}`, { json: { id, name } }).json<UpdateProjectResponse>()
+    );
+    return project;
   }
 
-  static async deleteProject(id: string): Promise<Project> {
-    return fetch(`${baseUrl}/projects/${id}`, {
-      ...deleteOptions,
-    })
-      .then((response) => response.json())
-      .then((response) => response);
+  static async deleteProject(id: string): Promise<void> {
+    await this.client.delete(`projects/${id}`);
   }
 
   static async getProjectUsers(id: string): Promise<User[]> {
-    return fetch(`${baseUrl}/projects/users/${id}`, getOptions)
-      .then((response) => response.json())
-      .then((response) => response.users);
+    const { users } = await this.handleApiError(
+      this.client.get(`projects/users/${id}`).json<GetProjectUsersResponse>()
+    ); // TODO - Change url structure to have id after projects
+    return users;
   }
 
   static async createProjectInvitation({
@@ -110,25 +131,23 @@ export class ApiClient {
     projectId: string;
     toUserEmail: string;
   }): Promise<ProjectInvitation> {
-    return fetch(`${baseUrl}/project-invitations`, {
-      ...postOptions,
-      body: JSON.stringify({ projectId, toUserEmail }),
-    })
-      .then((response) => response.json())
-      .then((response) => response.project_invitation);
+    const { project_invitation } = await this.handleApiError(
+      this.client
+        .post('project-invitations', { json: { projectId, toUserEmail } })
+        .json<CreateProjectInvitationResponse>()
+    );
+    return project_invitation;
   }
 
-  static async acceptProjectInvitation({ key }: { key: string }): Promise<void> {
-    await fetch(`${baseUrl}/project-invitations/${key}`, {
-      ...getOptions,
-    });
+  static async acceptProjectInvitation({ key }: { key: string }): Promise<AcceptProjectInvitationResponse> {
+    return this.handleApiError(this.client.get(`project-invitations/${key}`).json());
   }
 
   static async createStripeSession(): Promise<StripeSession> {
-    return fetch(`${baseUrl}/stripe/sessions`, {
-      ...postOptions,
-    })
-      .then((response) => response.json())
-      .then((response) => response.stripe_session);
+    const { stripe_session } = await this.handleApiError(
+      this.client.post('stripe/sessions').json<CreateStripeSessionResponse>()
+    );
+
+    return stripe_session;
   }
 }
