2bd3541a873971fc6718c913ab3729916b0de0ef
Karl Eriksson <keaeriksson@users.noreply.github.com>
2022-08-29 09:48:07 +0200

    Improve subscribe UI (#68)
    * Improve subscribe UI
    
    * service -> product
    
    * Rm unused import
    
    * Rm unused import

--- clients/web/src/pages/sign-in.tsx
+++ clients/web/src/pages/sign-in.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import {
-  Box,
   Text,
   useColorModeValue,
   Flex,
--- clients/web/src/pages/subscribe.tsx
+++ clients/web/src/pages/subscribe.tsx
@@ -11,14 +11,21 @@ import {
   AlertTitle,
   AlertDescription,
   AlertIcon,
+  Icon,
+  Grid,
+  GridItem,
 } from '@chakra-ui/react';
-import { MdVpnKey, MdPayments, MdBusinessCenter, MdLock, MdChevronRight, MdOutbound } from 'react-icons/md';
+import { MdVpnKey, MdPayments, MdBusinessCenter, MdLock, MdChevronRight } from 'react-icons/md';
+import { IoIosRocket } from 'react-icons/io';
 
 import { AuthenticatedLayout } from '../layouts';
 import { ApiClient } from '../lib/clients/api-client';
 import { useSearchParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { currentUser } from '../state';
+import { SoftCard } from '../components/generic/SoftCard';
+import { IconType } from 'react-icons';
+import { PageHeading } from '../components/generic/PageHeading';
 
 export const SubscribePage: React.FC = () => {
   const [searchParams] = useSearchParams();
@@ -33,9 +40,52 @@ export const SubscribePage: React.FC = () => {
     }
   };
 
+  type Feature = { icon: IconType; title: string; description: string };
+
+  const features = [
+    {
+      icon: MdVpnKey,
+      title: 'Authentication',
+      description: 'Secure and reliable authentication out of the box',
+    },
+    {
+      icon: MdPayments,
+      title: 'Payments',
+      description: 'Enable your users to subscribe to your product',
+    },
+    {
+      icon: MdBusinessCenter,
+      title: 'Projects',
+      description: 'Enable your users to create projects and invite their team',
+    },
+    {
+      icon: MdLock,
+      title: 'Secure',
+      description: 'Built with best practice security',
+    },
+  ] as Feature[];
+
+  const FeatureCard = ({ feature }: { feature: Feature }) => {
+    const { title, description, icon } = feature;
+    return (
+      <GridItem>
+        <SoftCard key={title} style={{ minHeight: '140px', display: 'flex', flexDirection: 'column' }}>
+          <Box>
+            <Heading size="md">
+              <Icon as={icon} /> {title}
+            </Heading>
+          </Box>
+
+          <Text>{description}</Text>
+        </SoftCard>
+      </GridItem>
+    );
+  };
+
   return (
     <AuthenticatedLayout sidebar={false}>
-      <Heading>{user?.free_trial_activated ? 'Subscribe' : 'Activate Free Trial'}</Heading>
+      <PageHeading title={user?.free_trial_activated ? 'Subscribe' : 'Activate Free Trial'} icon={IoIosRocket} />
+      <Box mt={8} />
       {searchParams.get('status') === 'warning' && (
         <Alert status="warning">
           <AlertIcon />
@@ -43,35 +93,15 @@ export const SubscribePage: React.FC = () => {
           <AlertDescription>Something went wrong while processing your payment. Please try again.</AlertDescription>
         </Alert>
       )}
-      <Text>
-        {user?.free_trial_activated
-          ? 'Subscribe to start using Volca.'
-          : 'Activate the 7-day free trial to start using Volca.'}
-      </Text>
-      <Box>
-        <List>
-          <ListItem>
-            <ListIcon as={MdVpnKey} />
-            <b>Authentication</b> - Secure and reliable authentication out of the box
-          </ListItem>
-          <ListItem>
-            <ListIcon as={MdPayments} />
-            <b>Subscriptions</b> - Enable your users to subscribe to your service
-          </ListItem>
-          <ListItem>
-            <ListIcon as={MdBusinessCenter} />
-            <b>Projects</b> - Enable your users to create projects to manage your features
-          </ListItem>
-          <ListItem>
-            <ListIcon as={MdLock} />
-            <b>Secure</b> - Built with best practice security
-          </ListItem>
-        </List>
-      </Box>
+      <Grid w="100%" templateColumns="repeat(4, 1fr)" gap={4}>
+        {features.map((feature) => (
+          <FeatureCard feature={feature} />
+        ))}
+      </Grid>
 
-      {!user?.free_trial_activated && (
-        <Box>
-          <List paddingTop="1em" paddingBottom="1em">
+      <Box pt={4} pb={4}>
+        {!user?.free_trial_activated && (
+          <List pb={4}>
             <ListItem>
               <ListIcon as={MdChevronRight} />
               After the free trial is over you will be charged $10 / month.
@@ -85,10 +115,23 @@ export const SubscribePage: React.FC = () => {
               You can cancel your subscription at any time.{' '}
             </ListItem>
           </List>
-        </Box>
-      )}
+        )}
 
-      <Button rightIcon={<MdOutbound />} colorScheme="blue" onClick={onActivate} type="submit" marginTop="1em">
+        <Text>
+          {user?.free_trial_activated
+            ? 'Subscribe to start using Volca.'
+            : 'Activate the 7-day free trial to start using Volca.'}
+        </Text>
+      </Box>
+
+      <Button
+        size="lg"
+        rightIcon={<IoIosRocket />}
+        colorScheme="blue"
+        onClick={onActivate}
+        type="submit"
+        marginTop="1em"
+      >
         {user?.free_trial_activated ? 'Subscribe' : 'Activate!'}
       </Button>
     </AuthenticatedLayout>
