cfa0d8a4c9fbc5d8dc52f39611f4406f044366ee
Oskar Karlsson <oskarkarlsson1993@gmail.com>
2022-09-24 18:21:25 +0200

    removes logging

--- services/api/src/actions/stripe/create-stripe-session.ts
+++ services/api/src/actions/stripe/create-stripe-session.ts
@@ -1,12 +1,11 @@
-import { CustomContext } from '../../types';
 import { container } from 'tsyringe';
 import { useApiAction } from '../utils/api-action';
 import { StripeService } from '../../services';
+import { User } from '../../entities';
 
-export const action = useApiAction(async (ctx: CustomContext) => {
+export const action = useApiAction(async () => {
   const stripeService = container.resolve(StripeService);
-
-  const { user } = ctx;
+  const user = container.resolve<User>('AuthenticatedUser');
 
   const stripeSession = await stripeService.createSession({ user });
 
