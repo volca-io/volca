83c1aaa326d0c420f5019933aeb72e7a81a9bf1c
keaeriksson <kea.eriksson@gmail.com>
2022-09-28 21:05:52 +0200

    Add exclude tags

--- services/api/src/types/router-context.ts
+++ services/api/src/types/router-context.ts
@@ -1,7 +1,11 @@
 import { RouterContext } from '@koa/router';
+/* volca-exclude-start os */
 import { User } from '../entities';
+/* volca-exclude-end os */
 
 export interface CustomContext extends RouterContext {
   correlationId: string;
+  /* volca-exclude-start os */
   user: User;
+  /* volca-exclude-end os */
 }
