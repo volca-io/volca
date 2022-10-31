6e2c46cb52cd85ef14ca27d5ff9a8d1a14e78a7a
Oskar Karlsson <Oskarkarlsson1993@gmail.com>
2022-10-31 22:19:19 +0100

    typescript configs for webapp (#110)

---
+++ clients/web/environment.ts
@@ -0,0 +1,25 @@
+import { config } from '../../volca.config';
+
+export enum Environment {
+  LOCAL = 'local',
+  STAGING = 'staging',
+  PRODUCTION = 'production',
+}
+
+type EnvironmentConfig = {
+  [env in Environment]: Record<string, string>;
+};
+
+export const environments: EnvironmentConfig = {
+  local: {
+    HOST: '127.0.0.1',
+    PORT: '3000',
+    REACT_APP_API_URL: 'http://127.0.0.1:4000/',
+  },
+  staging: {
+    REACT_APP_API_URL: `https://api.${config.environments.staging.domain}/`,
+  },
+  production: {
+    REACT_APP_API_URL: `https://api.${config.environments.staging.domain}/`,
+  },
+};
